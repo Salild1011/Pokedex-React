066d00a97e106125ca0c0dc13329df481a62a8c4
Salild1011 <salild1011@gmail.com>
2020-01-20 22:44:32 -0500

    Full window height used

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import axios from 'axios';
+
+
+const App = () => {
+  const [ selected, setSelected ] = useState(null);
+  const [ pokelist, setPokelist ] = useState([]);
+
+  const fetchAllPokemon = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => {
+        let data = response.data['results'];
+        setPokelist(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // Make network calls
+  useEffect(() => {
+    fetchAllPokemon();
+  }, [ pokelist.length ]);
+
+  let list = pokelist.map(value => {
+    return <li>{value.name}</li>;
+  });
+
   return (
     <div className="App">
-      
+      <div className="dex-container">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
